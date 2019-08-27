497dfcfe8acee32f6fa01b7a6e87cf02eb596761
interesting926 <995941767@qq.com>
2019-08-27 18:29:48 +0800

    关联

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -32,7 +32,7 @@ class PostController extends Controller
      */
     public function index()
     {
-       // dd(Post::all());
+        //dd(Post::all());
         return view('admin.post.index',['posts'=>Post::all()]);
     }
 
